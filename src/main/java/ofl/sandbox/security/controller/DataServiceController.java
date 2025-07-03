02b701d504958cf50f92b062e2a845b3eb0c919c
oflondontown <oflondontown@gmail.com>
2025-07-04 01:45:11 +0900

    NO-JIRA create a poc with microservices for auth data and webservice

--- src/main/java/ofl/sandbox/security/controller/DataServiceController.java
+++ src/main/java/ofl/sandbox/security/controller/DataServiceController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -25,7 +22,7 @@ public class DataServiceController {
         this.jwtService = jwtService;
     }
 
-    @GetMapping("/data")
+    @PostMapping("/data")
     @PreAuthorize("hasAuthority('CAN_SEE_DATA')") // this validates the user token
     public Mono<ResponseEntity<String>> data(
             @RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader, // user jwt
